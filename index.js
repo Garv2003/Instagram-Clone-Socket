d3a241ad1e2b6923bed6de2d13ece114b62d19a2
Garv05 <coolgarv9@gmail.com>
2024-02-21 02:51:07 +0530

    changes

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const compression = require("compression");
+const multer = require("multer");
 
 const app = express();
 const server = require("http").createServer(app);
+
+require("dotenv").config();
 
 app.use(
   cors({
@@ -20,7 +23,12 @@ app.use(compression());
 app.use(morgan("common"));
 app.use(express.json());
 
-require("dotenv").config();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 const io = socket(server, {
   cors: {
@@ -71,6 +79,17 @@ io.on("connection", (socket) => {
     io.to(user?.socketId).emit("typingResponse", data);
   });
 
+  app.post("/upload", upload.single("file"), (req, res) => {
+    console.log(req.body, req.file);
+    let user = users.find((user) => user.userId === req.body.receiverId);
+    io.to(user?.socketId).emit("getmessage", {
+      senderId: req.body.senderId,
+      text: req.body.text || "File uploaded",
+      createdAt: req.body.createdAt,
+    });
+    res.send("File uploaded");
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
     removeuser(socket.id);
@@ -78,6 +97,68 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+mongoose.connect(process.env.MONGODB_URl).then(() => {
+  console.log("Connected to MongoDB");
+  server.listen(process.env.PORT || 4444, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
 });
+
+// const socket = require("socket.io");
+// require("dotenv").config();
+
+// const io = socket(4444, {
+//   cors: {
+//     origin: process.env.CLIENT_URL,
+//     credentials: true,
+//   },
+// });
+
+// let users = [];
+
+// const adduser = (userId, socketId) => {
+//   if (!users.some((user) => user.userId === userId)) {
+//     users.push({ userId, socketId });
+//   }
+// };
+
+// const getuser = (userId) => {
+//   return users.find((user) => user.userId === userId);
+// };
+
+// const removeuser = (socketId) => {
+//   users = users.filter((user) => user.socketId !== socketId);
+// };
+
+// io.on("connection", (socket) => {
+//   console.log("user connected");
+//   socket.on("adduser", (userId) => {
+//     adduser(userId, socket.id);
+//     io.emit("getusers", users);
+//   });
+
+//   socket.on("joinroom", (roomId) => {
+//     socket.join(roomId);
+//     socket.broadcast.to(roomId).emit("userjoined", userId);
+//   });
+
+//   socket.on("sendmessageRoom", ({ senderId, roomId, text, createdAt }) => {
+//     io.to(roomId).emit("getmessageRoom", { senderId, text, createdAt });
+//   });
+
+//   socket.on("sendmessage", ({ senderId, receiverId, text, createdAt }) => {
+//     let user = users.find((user) => user.userId === receiverId);
+//     io.to(user?.socketId).emit("getmessage", { senderId, text, createdAt });
+//   });
+
+//   socket.on("typing", (data) => {
+//     let user = users.find((user) => user.userId === data.receiverId);
+//     io.to(user?.socketId).emit("typingResponse", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//     removeuser(socket.id);
+//     io.emit("getusers", users);
+//   });
+// });
